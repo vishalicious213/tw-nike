ba05260671d18cfaa4f4c09cff42b7170f492354
vishalicious213 <vishalicious213@gmail.com>
2023-11-14 12:20:51 -0800

    App: adding in placeholders for page sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 const App = () => (
-  <main className="relative"></main>
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+  </main>
 )
 
 export default App

7522d96a3ae3c1af5f923f1a85ec1f6d3c3ca464
vishalicious213 <vishalicious213@gmail.com>
2023-11-14 13:10:47 -0800

    App: rendered all section components

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,28 @@ const App = () => (
   <main className="relative">
     Nav
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer />
     </section>
     <section className="padding bg-pale-blue">
-      CustomerReviews
+      <CustomerReviews />
     </section>
     <section className="padding-x py-16 w-full sm:py-32">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer />
     </section>
   </main>
 )

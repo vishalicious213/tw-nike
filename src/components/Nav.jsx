2e33d7f086eddb4bd8045a200f249d2495e79aa6
vishalicious213 <vishalicious213@gmail.com>
2023-11-14 16:13:25 -0800

    Nav: imported hamburger menu icon and set height/width for logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import { headerLogo } from "../assets/images"
+import { hamburger } from "../assets/icons"
 
 const Nav = () => {
     return (
         <header>
             <nav>
                 <a href="/">
-                    <img src={headerLogo} alt="" />
+                    <img src={headerLogo} width={130} height={29} alt="Logo" />
                 </a>
             </nav>
         </header>

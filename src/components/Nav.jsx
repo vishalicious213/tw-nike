8da62c4b773306ea5c75e5973744a76b241623bc
vishalicious213 <vishalicious213@gmail.com>
2023-11-14 16:47:01 -0800

    Nav: added hamburger icon and hid it on large screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,10 +12,18 @@ const Nav = () => {
                 <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
                     {navLinks.map((item) => (
                         <li key={item.label}>
-                            <a href={item.href} className="font-montserrat leading-normal text-lg text=slate=gray">{item.label}</a>
+                            <a 
+                                href={item.href} 
+                                className="font-montserrat leading-normal text-lg text=slate=gray"
+                            >
+                                {item.label}
+                            </a>
                         </li>
                     ))}
                 </ul>
+                <div className="hidden max-lg:block">
+                    <img src={hamburger} alt="Hamburger" width={25} height={25} />
+                </div>
             </nav>
         </header>
     )

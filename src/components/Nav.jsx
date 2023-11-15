ce0656a4c2b7e07106e02a805a6df9b1a84abe23
vishalicious213 <vishalicious213@gmail.com>
2023-11-14 16:39:40 -0800

    Nav: rendering menu items for large screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,13 @@ const Nav = () => {
                 <a href="/">
                     <img src={headerLogo} width={130} height={29} alt="Logo" />
                 </a>
-                <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden"></ul>
+                <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                    {navLinks.map((item) => (
+                        <li key={item.label}>
+                            <a href={item.href} className="font-montserrat leading-normal text-lg text=slate=gray">{item.label}</a>
+                        </li>
+                    ))}
+                </ul>
             </nav>
         </header>
     )

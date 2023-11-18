896ecfc3becf82672e7c8965741e79d2b0c84285
vishalicious213 <vishalicious213@gmail.com>
2023-11-18 07:50:21 -0800

    ShoeCard: set up styles including dynamic border

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,7 +1,7 @@
-const ShoeCard = () => {
-  return (
-    <div>ShoeCard</div>
-  )
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImage }) => {
+    return (
+        <div className={`border-2 rounded-xl cursor-pointer max-sm:flex-1 ${bigShoeImage === imgURL ? "border-coral-red" : "border-transparent"}`}>ShoeCard</div>
+    )
 }
 
 export default ShoeCard

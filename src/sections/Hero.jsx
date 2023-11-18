bdcd7ccde9947478fb86cd5f77a30ecde452bdbd
vishalicious213 <vishalicious213@gmail.com>
2023-11-18 06:43:52 -0800

    Hero: added container div to #home

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,11 @@
 const Hero = () => {
   return (
-    <section id="home" className="w-full border-2 border-red-500 p-2"></section>
+    <section 
+      id="home" 
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28"></div>
+    </section>
   )
 }
 

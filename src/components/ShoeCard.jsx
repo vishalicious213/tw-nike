9e031ee7f03a931461c91a833e441cc0c7098906
vishalicious213 <vishalicious213@gmail.com>
2023-11-18 07:53:15 -0800

    ShoeCard: setting up onClick

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,6 +1,14 @@
 const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImage }) => {
+    const handleClick = () => {
+        console.log("clicked")
+    }
     return (
-        <div className={`border-2 rounded-xl cursor-pointer max-sm:flex-1 ${bigShoeImage === imgURL ? "border-coral-red" : "border-transparent"}`}>ShoeCard</div>
+        <div 
+            className={`border-2 rounded-xl cursor-pointer max-sm:flex-1 ${bigShoeImage === imgURL ? "border-coral-red" : "border-transparent"}`}
+            onClick={handleClick}
+        >
+            ShoeCard
+        </div>
     )
 }
 

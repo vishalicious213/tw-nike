1f8a5645a7699f2bf9d1f8a61981eeb592d9c956
vishalicious213 <vishalicious213@gmail.com>
2023-11-18 08:01:59 -0800

    ShoeCard: rendering shoe buttons

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -9,7 +9,9 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
             className={`border-2 rounded-xl cursor-pointer max-sm:flex-1 ${bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"}`}
             onClick={handleClick}
         >
-            ShoeCard
+            <div>
+                <img src={imgURL.thumbnail} alt="shoe collection" width={127} height={103} className="object-contain" />
+            </div>
         </div>
     )
 }

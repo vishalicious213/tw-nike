abba4f65b1bb74ccf92d09141ec68a46612d4343
vishalicious213 <vishalicious213@gmail.com>
2023-11-18 07:42:42 -0800

    drafted /src/components/ShoeCard.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import { arrowRight } from "../assets/icons"
 import { bigShoe1 } from "../assets/images"
 import Button from "../components/Button"
+import ShoeCard from "../components/ShoeCard"
 import { shoes, statistics } from "../constants"
 
 const Hero = () => {

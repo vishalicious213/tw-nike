3ad2d8d5f8c913f93c6e13972bc1650e73260e9e
vishalicious213 <vishalicious213@gmail.com>
2023-11-20 15:46:57 -0800

    ShoeCard: corrected issue that prevented border from appearing around selected shoe

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -6,7 +6,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
     }
     return (
         <div 
-            className={`border-2 rounded-xl cursor-pointer max-sm:flex-1 ${bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"}`}
+            className={`border-2 rounded-xl cursor-pointer max-sm:flex-1 ${bigShoeImg === imgURL.bigShoe ? "border-coral-red" : "border-transparent"}`}
             onClick={handleClick}
         >
             <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">

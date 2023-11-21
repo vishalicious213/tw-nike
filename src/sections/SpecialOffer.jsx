40a7270ac3c5c0b5a78f6d8ff82679884fcc598e
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 10:27:19 -0800

    SpecialOffer: imported and rendered button. pasted in content from SuperQuality to modify

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button"
 import { offer } from "../assets/images"
 
 const SpecialOffer = () => {
@@ -5,6 +6,19 @@ const SpecialOffer = () => {
     <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
       <div className="flex-1">
         <img src={offer} alt="" width={773} height={687} className="object-contain w-full" />
+      </div>
+
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You<span className="text-coral-red"> Super Quality</span> Shoes
+        </h2>
+
+        <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously-crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
+        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction.</p>
+
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
       </div>
     </section>
   )

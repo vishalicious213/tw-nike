815f1751c60746da1674fcccb886ba04ebb0c7e2
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 12:21:55 -0800

    CustomerReviews: added section, h3 and styled them

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,6 +1,8 @@
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">What Our <span className="text-coral-red">Customers</span> Say</h3>
+    </section>
   )
 }
 

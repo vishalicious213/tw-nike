66e651a84c9b08ab08844c7dd403537903dfe167
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 07:40:38 -0800

    PopularProductCard: rendering rating for each shoe. modified constants to include rating data

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,12 +1,13 @@
 import { star } from "../assets/icons"
 
-const PopularProductCard = ({ imgURL, name, price }) => {
+const PopularProductCard = ({ imgURL, name, price, rating }) => {
     return (
         <div className="flex flex-1 flex-col w-full max-sm:w-full">
             <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
             
             <div className="mt-8 flex justify-start gap-2 5">
                 <img src={star} alt="rating" width={24} height={24} />
+                <p>{rating}</p>
             </div>
         </div>
 

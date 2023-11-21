223d8afc6e60ea96ffe9d8c42f7c85f6f221bd20
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 14:25:41 -0800

    Subscribe: added styles to the input container

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -5,7 +5,7 @@ const Subscribe = () => {
     <section id="contact-us" className="max-container flex justify-between items-center gap-10 max-lg:flex-col">
       <h3 className="text-4xl leading-[68px] font-palanquin font-bold lg:max-w-md ">Sign Up For <span className="text-coral-red">Updates</span> & Newsletter</h3>
       
-      <div>
+      <div className="w-full flex items-center gap-5 p-2.5 lg:max-w-[40%] max-sm:flex-col sm:border sm:border-slate-gray rounded-full">
         <input type="text" className="input" placeholder="subscribe@nike.com"></input>
         <div className="flex items-center max-sm:justify-end max-sm:w-full">
           <Button label="Sign Up" fullWidth />

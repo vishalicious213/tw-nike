99a7545e172384e6e963242acbc74d8f6f511637
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 07:59:22 -0800

    SuperQuality: added section and its tailwind styles

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,8 @@
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+      
+    </section>
   )
 }
 

4c9f19c455a5bfe83c9159b6c6587551090c12b0
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 08:55:33 -0800

    SpecialOffer: imported and rendered offer image

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,12 @@
+import { offer } from "../assets/images"
+
 const SpecialOffer = () => {
   return (
-    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container"></section>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} alt="" width={773} height={687} className="object-contain w-full" />
+      </div>
+    </section>
   )
 }
 

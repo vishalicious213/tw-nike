df8072bb8cb8aaa4798407999870fc6aaeb9c854
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 07:46:23 -0800

    PopularProductCard: added show name and price. styled them

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -9,6 +9,8 @@ const PopularProductCard = ({ imgURL, name, price, rating }) => {
                 <img src={star} alt="rating" width={24} height={24} />
                 <p className="font-montserrat text-xl leading-normal text-slate-gray">{`(${rating})`}</p>
             </div>
+            <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
+            <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">{price}</p>
         </div>
 
     )

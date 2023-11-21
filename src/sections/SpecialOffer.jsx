d0608876cf590ad9b2ec54483d5c13e7f7c1e4dc
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 10:32:17 -0800

    SpecialOffer: updated button and imported arrowRight

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button"
 import { offer } from "../assets/images"
+import { arrowRight } from "../assets/icons"
 
 const SpecialOffer = () => {
   return (
@@ -16,8 +17,8 @@ const SpecialOffer = () => {
         <p className="mt-4 lg:max-w-lg info-text">Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.</p>
         <p className="mt-6 lg:max-w-lg info-text">Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.</p>
 
-        <div className="mt-11">
-          <Button label="View details" />
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Show now" iconURL={arrowRight} />
         </div>
       </div>
     </section>

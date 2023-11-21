fa1dcc8c065f17f715610397e833a84276427776
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 07:34:19 -0800

    PopularProductCard: destructured product data, added tailwind classes, rendered image per card

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,7 +1,9 @@
-const PopularProductCard = () => {
-  return (
-    <div>PopularProductCard</div>
-  )
+const PopularProductCard = ({ imgURL, name, price }) => {
+    return (
+        <div className="flex flex-1 flex-col w-full max-sm:w-full">
+            <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+        </div>
+    )
 }
 
 export default PopularProductCard

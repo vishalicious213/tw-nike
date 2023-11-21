f970c9d714db1300722c8047bfc6824a729ea24e
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 13:33:58 -0800

    ReviewCard: imported and rendered star for customer ratings

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,3 +1,5 @@
+import { star } from "../assets/icons"
+
 const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
   return (
     <div className="flex justify-center items-center flex-col">
@@ -7,6 +9,13 @@ const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
             className="rounded-full object-cover w-[120px] h-[120px]"
         />
     <p>{feedback}</p>
+    <img 
+        src={star}
+        width={24}
+        height={24}
+        className="object-contain m-0"
+        alt="rating" 
+    />
     </div>
   )
 }

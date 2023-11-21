78b27517cfab4cf4f96b66852dadc4662c3f517e
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 08:26:06 -0800

    Services: imported services from constants and mapped through them, rendering a ServiceCard for each

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -2,7 +2,11 @@ import { services } from "../constants"
 
 const Services = () => {
   return (
-    <section className="max-container flex justify-center flex-wrap gap-9"></section>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map(service => {
+        <ServiceCard key={service.label} {...service} />
+      })}
+    </section>
   )
 }
 

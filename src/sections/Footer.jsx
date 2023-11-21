579638177245d5e42f2c31589c305dcaba5b4d62
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 15:52:47 -0800

    Footer: added logo as link to home

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,14 @@
+import { footerLogo } from "../assets/images"
+
 const Footer = () => {
   return (
     <footer className="max-container">
       <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
-
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="" width={150} height={46} />
+          </a>
+        </div>
       </div>
     </footer>
   )

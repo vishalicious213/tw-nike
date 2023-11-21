00700242171f6dc45b59fc51ddcf5a85799e8eb7
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 14:21:31 -0800

    Subscribe: added styles to the button container

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -2,12 +2,12 @@ import Button from "../components/Button"
 
 const Subscribe = () => {
   return (
-    <section id="contact-us" className="max-container flex justify-between items-center max-lg:flex-col gap-10">
-      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up For <span className="text-coral-red">Updates</span> & Newsletter</h3>
+    <section id="contact-us" className="max-container flex justify-between items-center gap-10 max-lg:flex-col">
+      <h3 className="text-4xl leading-[68px] font-palanquin font-bold lg:max-w-md ">Sign Up For <span className="text-coral-red">Updates</span> & Newsletter</h3>
       
       <div>
         <input type="text" className="input" placeholder="subscribe@nike.com"></input>
-        <div>
+        <div className="flex items-center max-sm:justify-end max-sm:w-full">
           <Button label="Sign Up" fullWidth />
         </div>
       </div>

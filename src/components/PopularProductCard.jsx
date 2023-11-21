6b82b00d9406f7ba50f37a90982962351b937ba6
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 07:42:56 -0800

    PopularProductCard: styled rating

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -7,7 +7,7 @@ const PopularProductCard = ({ imgURL, name, price, rating }) => {
             
             <div className="mt-8 flex justify-start gap-2 5">
                 <img src={star} alt="rating" width={24} height={24} />
-                <p>{rating}</p>
+                <p className="font-montserrat text-xl leading-normal text-slate-gray">{`(${rating})`}</p>
             </div>
         </div>
 

18163ef230a6cb87aecf784f3332f8812673f3ac
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 13:25:26 -0800

    CustomerReviews: passing props to ReviewCard

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -9,7 +9,13 @@ const CustomerReviews = () => {
 
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
         {reviews.map(review => (
-          <ReviewCard key={review.customerName} />
+          <ReviewCard 
+            key={review.customerName} 
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
         ))}
       </div>
     </section>

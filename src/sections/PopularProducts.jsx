5c24bbe919ede4b54463aad180477066ecf59a0f
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 07:30:19 -0800

    PopularProducts: rendering PopularPRoductCard for each product. drafted /src/components/PopularProductCard

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,4 +1,5 @@
 import { products } from "../constants"
+import PopularProductCard from "../components/PopularProductCard"
 
 const PopularProducts = () => {
   return (
@@ -8,7 +9,11 @@ const PopularProducts = () => {
         <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design and value.</p>
       </div>
 
-      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14"></div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map(product => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
     </section>
   )
 }

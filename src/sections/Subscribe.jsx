c4e4de2f5fb7f27e49ccf88a1d6df24ab8d462cb
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 13:58:07 -0800

    Subscribe: added h3 and styled it

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,7 @@
 const Subscribe = () => {
   return (
     <section id="contact-us" className="max-container flex justify-between items-center max-lg:flex-col gap-10">
-      
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up For <span className="text-coral-red">Updates</span> & Newsletter</h3>
     </section>
   )
 }

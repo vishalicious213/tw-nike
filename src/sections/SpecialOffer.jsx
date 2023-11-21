e7a6cac6c111cf8f3ba313990d3eefe16f97abda
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 10:30:39 -0800

    SpecialOffer: updated h2 and p text

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -10,11 +10,11 @@ const SpecialOffer = () => {
 
       <div className="flex flex-1 flex-col">
         <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
-          We Provide You<span className="text-coral-red"> Super Quality</span> Shoes
+          <span className="text-coral-red">Special</span> Offer
         </h2>
 
-        <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously-crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
-        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction.</p>
+        <p className="mt-4 lg:max-w-lg info-text">Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.</p>
+        <p className="mt-6 lg:max-w-lg info-text">Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.</p>
 
         <div className="mt-11">
           <Button label="View details" />

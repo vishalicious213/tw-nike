bababc5cf95c178feca62a14cd0989e6622c26a7
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 08:40:57 -0800

    ServiceCard: rendering label and subtext

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,6 +4,8 @@ const ServiceCard = ({ imgURL, label, subtext }) => {
             <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full">
                 <img src={imgURL} alt={label} width={24} height={24} />
             </div>
+            <h3>{label}</h3>
+            <p>{subtext}</p>
         </div>
     )
 }

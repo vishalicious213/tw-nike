3c6b7d312a6527c009e90b52ec4b6faca2c2670d
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 13:30:51 -0800

    ReviewCard: rendered and styled customer images

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,7 +1,12 @@
 const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
   return (
     <div className="flex justify-center items-center flex-col">
-        
+        <img 
+            src={imgURL} 
+            alt="customer" 
+            className="rounded-full object-cover w-[120px] h-[120px]"
+        />
+
     </div>
   )
 }

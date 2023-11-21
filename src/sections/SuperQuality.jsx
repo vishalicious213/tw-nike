7c6c404b3296e48bfb63b0d57c9af5c83675e87b
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 08:02:26 -0800

    SuperQuality: imported Button and brought in elements from Hero to modify

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,17 @@
+import Button from "../components/Button"
+
 const SuperQuality = () => {
   return (
     <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
-      
+      <div className="flex flex-1 flex-col">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort and innovation for your active life.</p>
+        <Button label="Shop now" />
+      </div>
     </section>
   )
 }

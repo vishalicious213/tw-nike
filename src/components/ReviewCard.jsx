97f155a78d62822286025b9a20366900ac72cadb
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 13:38:01 -0800

    ReviewCard: added rating container and styled it

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -8,15 +8,17 @@ const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
             alt="customer" 
             className="rounded-full object-cover w-[120px] h-[120px]"
         />
-    <p>{feedback}</p>
-    <img 
-        src={star}
-        width={24}
-        height={24}
-        className="object-contain m-0"
-        alt="rating" 
-    />
-    <p>({rating})</p>
+    <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+    <div className="mt-3 flex justify-center items-center gap-2 5">
+        <img 
+            src={star}
+            width={24}
+            height={24}
+            className="object-contain m-0"
+            alt="rating" 
+        />
+        <p>({rating})</p>
+    </div>
     <h3>{customerName}</h3>
     </div>
   )

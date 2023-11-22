fb3ae5c5bf16c55c91cc009c0e146950fa570ca5
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 16:28:56 -0800

    Footer: added container for copyright img

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -33,7 +33,9 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center"></div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer"></div>
+      </div>
     </footer>
   )
 }

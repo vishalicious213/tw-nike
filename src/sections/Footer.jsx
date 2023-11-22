afb4a9736341b8550f36c7bfa7cb4a2ce2cae301
vishalicious213 <vishalicious213@gmail.com>
2023-11-21 16:15:18 -0800

    Footer: created div for footer links

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,5 @@
 import { footerLogo } from "../assets/images"
-import { socialMedia } from "../constants"
+import { footerLinks, socialMedia } from "../constants"
 
 const Footer = () => {
   return (
@@ -12,11 +12,19 @@ const Footer = () => {
           <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">Get shoes ready  for the new term at your nearest Nike store. Find Your perfect size in store. Get rewards!</p>
           <div className="flex items-center gap-5 mt-8">
             {socialMedia.map(icon => (
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+              <div key={icon.src} className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
               </div>
             ))}
           </div>
+        </div>
+
+        <div className="flex flex-1 justify-between gap-20 flex-wrap lg:gap-10">
+          {footerLinks.map(section => (
+            <div key={section.title}>
+
+            </div>
+          ))}
         </div>
       </div>
     </footer>
